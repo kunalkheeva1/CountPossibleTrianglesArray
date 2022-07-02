7e9de023c1b118033b83f429489dae1d5310f738
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-01 19:35:27 -0700

    created a method to identify the triplets that make triangles from an array

--- src/CountPossibleTriangleArray.java
+++ src/CountPossibleTriangleArray.java
@@ -1,5 +1,21 @@
 public class CountPossibleTriangleArray {
-    
+    public static int countPossibleTriangleArray( int arr [], int n){
+        int count = 0;
+        int i, j, k;
+        for(k = n-1; k>0; k++){
+            i = 0;
+            j = k-1;
+            while(i<j){
+                if(arr[i] + arr[j] >arr[k]){
+                    count += (j-i);
+                    j--;
+                }else{
+                    i++;
+                }
+            }
+
+        }return count;
+    }
 
     public static void main(String[] args) {
 
